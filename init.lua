049baa0436c6919078b18cf8e22523d633f28a78
k <hacken@zhonghaiweb.com>
2023-08-07 14:59:59 +0800

    基础配置结束

--- init.lua
+++ init.lua
@@ -17,11 +17,19 @@ vim.api.nvim_create_autocmd({ "TextYankPost" },{
 
 
 -- key binding 
+local opt = { noremap = true, silent = true}
+
+vim.g.mapleader = " "
+
 
 vim.keymap.set("n","<C-l>","<C-w>l",opt)
 vim.keymap.set("n","<C-h>","<C-w>h",opt)
 vim.keymap.set("n","<C-j>","<C-w>j",opt)
 vim.keymap.set("n","<C-k>","<C-w>k",opt)
+vim.keymap.set("n","<leader>v","<C-w>v",opt)
+vim.keymap.set("n","<leader>s","<C-w>s",opt)
+vim.keymap.set("n","k",[[v:count ? 'k':'gk']],{ noremap = true,expr = true })
+vim.keymap.set("n","j",[[v:count ? 'j':'gj']],{ noremap = true,expr = true })
 
 
 
